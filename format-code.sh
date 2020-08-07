50925f7469182b67c90c0c84300c96ca3b55114f
Allen Calderwood <allen@hicharlie.co>
2020-08-07 00:56:13 -0700

    Test

--- format-code.sh
+++ format-code.sh
@@ -3,10 +3,19 @@ mkdir -p .cache
 cd .cache
 if [ ! -f google-java-format-1.8-all-deps.jar ]
 then
+    echo "curl"
     curl -LJO "https://github.com/google/google-java-format/releases/download/google-java-format-1.8/google-java-format-1.8-all-deps.jar"
+    pwd
+    ls
+    echo "chmod"
     chmod 755 google-java-format-1.8-all-deps.jar
+    pwd
+    ls
 fi
 cd ..
+
+pwd
+ls
 
 changed_java_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*java$" )
 echo $changed_java_files
